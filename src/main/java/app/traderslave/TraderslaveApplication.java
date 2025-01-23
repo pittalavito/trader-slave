885ca119d5863b64c2f7391e3e515ddf07b0c4b1
pittalavito <81303388+pittalavito@users.noreply.github.com>
2025-01-23 13:06:31 +0100

    Added empty spring project

---
+++ src/main/java/app/traderslave/TraderslaveApplication.java
@@ -0,0 +1,13 @@
+package app.traderslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderslaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderslaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/traderslave/TraderSlaveApplicationTests.java
@@ -0,0 +1,14 @@
+package app.traderslave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraderSlaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// [sonar_issue_tag]
+	}
+
+}
