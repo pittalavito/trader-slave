d9eab329a89d7fa763a10fa388730b67bc56fbe6
pittalavito <81303388+pittalavito@users.noreply.github.com>
2025-02-02 15:35:16 +0100

    Added method to time-utils class

--- src/main/java/app/traderslave/utility/TimeUtils.java
+++ src/main/java/app/traderslave/utility/TimeUtils.java
@@ -1,8 +1,6 @@
 package app.traderslave.utility;
 
-import jakarta.validation.constraints.NotNull;
 import lombok.experimental.UtilityClass;
-
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -10,14 +8,20 @@ import java.time.ZoneOffset;
 @UtilityClass
 public class TimeUtils {
 
-    public Long convertToUTCMillisecond(@NotNull LocalDateTime localDateTime) {
-        return localDateTime.toEpochSecond(ZoneOffset.UTC) * 1000;
+    public Long convertToUTCMillisecondOrDefault(LocalDateTime localDateTime, Long defaultValue) {
+        return localDateTime == null ? defaultValue : convertToUTCMillisecond(localDateTime);
     }
 
-    public LocalDateTime convertToUTCLocalDate(@NotNull Long millisecond) {
+    public Long convertToUTCMillisecond(LocalDateTime localDateTime) {
+        return localDateTime
+                .toEpochSecond(ZoneOffset.UTC) * 1000;
+    }
+
+    public LocalDateTime convertToUTCLocalDate(Long millisecond) {
         return Instant
                 .ofEpochMilli(millisecond)
                 .atZone(ZoneOffset.UTC)
                 .toLocalDateTime();
     }
+
 }
