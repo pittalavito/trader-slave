0493c369b4f1ca51fcefe558c45396699caad7b1
pittalavito <81303388+pittalavito@users.noreply.github.com>
2025-02-18 20:42:13 +0100

    [Temporary commit] Remove test

--- src/test/java/app/traderslave/TraderslaveApplicationTests.java
+++ src/test/java/app/traderslave/TraderslaveApplicationTests.java
@@ -1,14 +1,8 @@
 package app.traderslave;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TraderslaveApplicationTests {
-
-	@Test
-	void contextLoads() {
-		// [sonar_issue_tag]
-	}
 
 }
