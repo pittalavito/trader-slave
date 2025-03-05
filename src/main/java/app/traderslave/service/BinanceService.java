83b2f190a4f3c1cf9051c8b2cea469f5f66a9fac
pittalavito <81303388+pittalavito@users.noreply.github.com>
2025-03-05 13:16:22 +0100

    Refactored binance-service

--- src/main/java/app/traderslave/service/BinanceService.java
+++ src/main/java/app/traderslave/service/BinanceService.java
@@ -54,46 +54,49 @@ public class BinanceService {
         TimeChecker.checkDateOrder(order.getOpenTime(), endTime);
         Duration duration = Duration.between(order.getOpenTime(), endTime);
 
-        if(duration.toHours() <=  180) {
-            return findCandles(BinanceServiceAdapter.adapt(order.getOpenTime(), simulation, endTime, TimeFrame.ONE_MINUTE))
-                    .map(candlesRes -> ReportOrderFactory.create(order, candlesRes));
+        if (duration.toHours() <= 180) {
+            return createShortTermReport(simulation, order, endTime);
         }
 
-        return createReportForCaseMore180DaysStepOne(simulation, order, endTime);
+        return createLongTermReport(simulation, order, endTime);
     }
 
-    private Mono<ReportOrder> createReportForCaseMore180DaysStepOne(Simulation simulation, SimulationOrder order, LocalDateTime endTime) {
-        LocalDateTime midnightAfterOpening = order.getOpenTime().plusDays(1).toLocalDate().atStartOfDay();
+    private Mono<ReportOrder> createShortTermReport(Simulation simulation, SimulationOrder order, LocalDateTime endTime) {
+        return findCandles(BinanceServiceAdapter.adapt(order.getOpenTime(), simulation, endTime, TimeFrame.ONE_MINUTE))
+                .map(candlesRes -> ReportOrderFactory.create(order, candlesRes));
+    }
 
+    private Mono<ReportOrder> createLongTermReport(Simulation simulation, SimulationOrder order, LocalDateTime endTime) {
+        LocalDateTime midnightAfterOpening = order.getOpenTime().plusDays(1).toLocalDate().atStartOfDay();
         return findCandles(BinanceServiceAdapter.adapt(order.getOpenTime(), simulation, midnightAfterOpening, TimeFrame.ONE_MINUTE))
                 .flatMap(candlesRes -> {
                     ReportOrder report = ReportOrderFactory.create(order, candlesRes);
-                    return report.isLiquidated() ?
-                            Mono.just(report) :
-                            createReportForCaseMore180DaysStepTwo(simulation, order, midnightAfterOpening, endTime, report);
+                    return report.isLiquidated() ? Mono.just(report) : createLongTermReportStepTwo(simulation, order, midnightAfterOpening, endTime, report);
                 });
     }
 
-    private Mono<ReportOrder> createReportForCaseMore180DaysStepTwo(Simulation simulation, SimulationOrder order, LocalDateTime midnightAfterOpening, LocalDateTime endTime, ReportOrder rep1) {
+    private Mono<ReportOrder> createLongTermReportStepTwo(Simulation simulation, SimulationOrder order, LocalDateTime midnightAfterOpening, LocalDateTime endTime, ReportOrder rep1) {
         LocalDateTime midnightBeforeEndTime = endTime.minusDays(1).toLocalDate().atStartOfDay();
-
         return findCandles(BinanceServiceAdapter.adapt(midnightAfterOpening, simulation, midnightBeforeEndTime, TimeFrame.ONE_DAY))
                 .flatMap(candlesRes -> {
-                    ReportOrder repTimeFrameOneDay =  ReportOrderFactory.create(order, candlesRes, rep1);
+                    ReportOrder repTimeFrameOneDay = ReportOrderFactory.create(order, candlesRes, rep1);
                     if (repTimeFrameOneDay.isLiquidated()) {
-                        LocalDateTime liquidationDay = repTimeFrameOneDay.getCloseTime().toLocalDate().atStartOfDay();
-                        Duration duration = Duration.between(liquidationDay, endTime);
-                        LocalDateTime closeTime = duration.toHours() < 25 ? endTime : liquidationDay.toLocalDate().atTime(23, 59, 59, 999);
-                        return findCandles(BinanceServiceAdapter.adapt(liquidationDay, simulation, closeTime, TimeFrame.ONE_MINUTE))
-                                .map(candlesRes2 -> ReportOrderFactory.create(order, candlesRes2, repTimeFrameOneDay));
+                        return handleLiquidationCreateLongTermReportStepTwo(simulation, order, endTime, repTimeFrameOneDay);
                     }
-
                     ReportOrder rep2 = ReportOrderFactory.create(order, candlesRes, repTimeFrameOneDay);
-                    return createReportForCaseMore180DaysStepThree(simulation, order, midnightBeforeEndTime, endTime, rep2);
+                    return createLongTermReportStepThree(simulation, order, midnightBeforeEndTime, endTime, rep2);
                 });
     }
 
-    private Mono<ReportOrder> createReportForCaseMore180DaysStepThree(Simulation simulation, SimulationOrder order, LocalDateTime midnightBeforeEndTime, LocalDateTime endTime, ReportOrder rep1) {
+    private Mono<ReportOrder> handleLiquidationCreateLongTermReportStepTwo(Simulation simulation, SimulationOrder order, LocalDateTime endTime, ReportOrder repTimeFrameOneDay) {
+        LocalDateTime liquidationDay = repTimeFrameOneDay.getCloseTime().toLocalDate().atStartOfDay();
+        Duration duration = Duration.between(liquidationDay, endTime);
+        LocalDateTime closeTime = duration.toHours() < 25 ? endTime : liquidationDay.toLocalDate().atTime(23, 59, 59, 999);
+        return findCandles(BinanceServiceAdapter.adapt(liquidationDay, simulation, closeTime, TimeFrame.ONE_MINUTE))
+                .map(candlesRes2 -> ReportOrderFactory.create(order, candlesRes2, repTimeFrameOneDay));
+    }
+
+    private Mono<ReportOrder> createLongTermReportStepThree(Simulation simulation, SimulationOrder order, LocalDateTime midnightBeforeEndTime, LocalDateTime endTime, ReportOrder rep1) {
         return findCandles(BinanceServiceAdapter.adapt(midnightBeforeEndTime, simulation, endTime, TimeFrame.ONE_MINUTE))
                 .map(candlesRes -> ReportOrderFactory.create(order, candlesRes, rep1));
     }
@@ -103,7 +106,6 @@ public class BinanceService {
                 .flatMapMany(clientRes -> {
                     if (!CollectionUtils.isEmpty(clientRes)) {
                         accumulatedCandlesList.addAll(BinanceApiResponseAdapter.adapt(clientRes));
-                        // close time last candle response in millisecond + 1
                         clientReqDto.setStartTime(((Long) clientRes.get(clientRes.size() - 1)[6]) + 1);
                         return fetchCandleSticks(accumulatedCandlesList, clientReqDto);
                     }
@@ -111,5 +113,3 @@ public class BinanceService {
                 });
     }
 }
-
-
